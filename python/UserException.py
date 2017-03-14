45df7d1ec958aa3f6ae8220042063244a5f8fd8c
jax79sg <jax79sg@yahoo.com.sg>
2017-03-14 16:04:07 +0000

    Added UserException classes

---
+++ python/UserException.py
@@ -0,0 +1,13 @@
+
+class ModelNotTrainedException(Exception):
+    """Exception raised for untrained models.
+    Changelog:
+    - 14/3 KS First commit
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
