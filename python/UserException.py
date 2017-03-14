2ec154094912bbf306c8dcd6fde92b14dd82751a
jax79sg <jax79sg@yahoo.com.sg>
2017-03-14 21:56:18 +0000

    Add UserExceptions

--- python/UserException.py
+++ python/UserException.py
@@ -11,3 +11,17 @@ class ModelNotTrainedException(Exception):
     def __init__(self, expression, message):
         self.expression = expression
         self.message = message
+
+
+class InvalidDatasetException(Exception):
+    """Exception raised for invalid datasets.
+    Changelog:
+    - 14/3 KS First commit
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message
