1a0743f92b87851198916631443824270d484f9a
jax79sg <jax79sg@yahoo.com.sg>
2017-03-15 23:49:21 +0000

    Added method to create new bm25 column

--- python/Feature_BM25.py
+++ python/Feature_BM25.py
@@ -18,6 +18,35 @@ class Feature_BM25():
     colProductId='product_id'
     colContent='content'
 
+    def getBM25(self,df):
+        """
+        Meant to be use in panda apply.
+        See below
+        :param df:
+        :return:
+        """
+        productid = df['product_uid']
+        search_term = df['search_term'].split()
+        return self.score(search_term, productid)
+
+    def computeBM25Column(self,trainset, colName='bm25'):
+        """
+        Compute a new bm25 column given a dataframe
+        Changelog
+        - 15/3 KS First commit
+        :param trainset: Training Dataframe, should contain  product_uid and search_term columns
+        :param colName: name of the new column
+        :return:
+        """
+        if(~isinstance(trainset,pd.DataFrame)):
+            if(trainset.shape[1]<2):
+                if ('product_uid' not in list(trainset) or 'search_term' not in list(trainset)):
+                    raise InvalidDatasetException("Invalid Dataframe for BM25 compute","Expecting A Pandas.Dataframe with columns 'product_uid' and 'search_term' ")
+
+        #Apply scoring function across each row of dataframe
+        trainset[colName]=trainset.apply(self.getBM25,axis=1)
+        return trainset
+
 
     def __getVectorForDocument(self,document):
         """
@@ -42,11 +71,11 @@ class Feature_BM25():
         """
         #Preprocessing the text
         dp = DataPreprocessing()
-        text = dp.getBagOfWords(documentDF=documents['content'], return_type='document_tokens')
+        text = dp.getBagOfWords(documentDF=documents, return_type='document_tokens')
 
         #Create a Gensim text corpus based on documents
         print("Creating a text dictionary")
-        self.dictionary=Dictionary(line.lower().split() for line in documents['content'])
+        self.dictionary=Dictionary(line.lower().split() for line in documents)
         print(self.dictionary)
         print("Saving text dictionary to file")
         self.dictionary.save('../data.prune/producttext.dict')
@@ -67,23 +96,23 @@ class Feature_BM25():
         Initialise the BM25 corpus with words of all documents
         :param documents: A Dataframe 2 columns. One of 'product_id', other of 'content'
         """
-        print("Initialising BM25...this may take a while")
+        print("Initialising BM25...")
         if(~isinstance(documents,pd.DataFrame)):
-            if(documents.shape[1]!=2):
-                if (list(documents)[0] != 'product_id' or list(documents)[1] != 'content'):
-                    raise InvalidDatasetException("Invalid Dataframe for BM25 init","Expecting A Pandas.Dataframe with columns 'product_id' and 'content' only")
+            if(documents.shape[1]<2):
+                if ('product_uid' not in list(documents) or 'content' not in list(documents)):
+                    raise InvalidDatasetException("Invalid Dataframe for BM25 init","Expecting A Pandas.Dataframe with columns 'product_uid' and 'content' ")
 
         #Mapping of product_id to indices of BM25 documents
         print("Mapping product_id to corpus index")
         counter=0
-        for productid in documents['product_id']:
+        for productid in documents['product_uid']:
             self.productDict[productid]=counter
             counter=counter+1
 
 
         #Convert documents into corpus
         print("Converting documents into corpus")
-        self.__convertToCorpus(documents)
+        self.__convertToCorpus(documents['content'])
 
         #Initialise the BM25 computation
         print("Computing BM25 baselines")
@@ -92,19 +121,39 @@ class Feature_BM25():
 
         print("self.bm25 corpus_size",self.bm25.corpus_size)
         print("self.bm25 avgdl", self.bm25.avgdl)
-        print("self.bm25 f", self.bm25.f)
-        print("self.bm25 df", self.bm25.df)
-        print("self.bm25 idf", self.bm25.idf)
+        # print("self.bm25 f", self.bm25.f)
+        # print("self.bm25 df", self.bm25.df)
+        # print("self.bm25 idf", self.bm25.idf)
         print("self.bm25 avgIDF", self.avgIDF)
         print("self.bm25 corpus", self.bm25.corpus)
 
 
         print("========Initialising BM25...completed...")
 
+    def score(self,query,document):
+        """
+        BM25 will return BM25(query1, doc1).
+        if documnent doesn't exists, it will be ignored.
+        Changelog
+        - 15/3 KS First commit
+        :param query: Expecting a list of words. ['One','Two','Three']
+        :param document: A  'product_id'
+        :return:
+        """
+        #Convert query into vector
+        print("===== Single scoring BM25 for query ",query, " against documents ",document)
+        queryVector = self.__getVectorForDocument(query)[0]
+        print("queryVector:",queryVector)
 
+        # Find out the index of the document in the document corpus
+        documentindex = self.productDict[document]
 
+        score=self.bm25.get_score(queryVector, documentindex, self.avgIDF)
+        print("score:",score)
+        print("===== Scoring BM25 for query completed")
+        return score
 
-    def score(self,query,documents):
+    def scores(self,query,documents):
         """
         BM25 will return BM25(query1, doc1), BM25(query1, doc2), BM25(query1, doc3), BM25(query2, doc1)...
         if documnent doesn't exists, it will be ignored.
@@ -115,30 +164,35 @@ class Feature_BM25():
         :return: A list of scores in the order of the given documents
         """
 
-        #Convert query into vector
-        print("===== Scoring BM25 for query ",query, " against documents ",documents)
-        queryVector = self.__getVectorForDocument(query)[0]
-        print("queryVector:",queryVector)
+
+
+        # #Convert query into vector
+        print("===== Multiple scoring BM25 for query ",query, " against documents ",documents)
+        # queryVector = self.__getVectorForDocument(query)[0]
+        # print("queryVector:",queryVector)
 
         #Find out the indices of the documents in the document corpus
         bm25_scores=[]
         for productid in documents:
-            documentindex=self.productDict[productid]
-            print("documentindex found:", documentindex)
+            # documentindex=self.productDict[productid]
+            # print("documentindex found:", documentindex)
             # Run the scoring function for the query against each document
-            bm25_scores.append(self.bm25.get_score(queryVector,documentindex,self.avgIDF))
+            # bm25_scores.append(self.bm25.get_score(queryVector,documentindex,self.avgIDF))
+            print("Product ID:",productid)
+            bm25_scores.append(self.score(query,productid))
 
         print("===== Scoring BM25 for query completed")
+        print("bm25_scores:",bm25_scores)
         return bm25_scores
 
 if __name__ == "__main__":
     #Test documents
     a = [[10001, 'hello world'], [10002, 'memme too too'], [10003, 'goto\nhello there again world'],[10004,'I am working too']]
-    df = pd.DataFrame(a,columns=['product_id','content'])
+    df = pd.DataFrame(a,columns=['product_uid','content'])
     #Initialise model
     bm25 = Feature_BM25(df)
     #Test Query
     q=['too','too','unknownword']
-    print(bm25.score(q,[10001,10002,10004]))
+    print(bm25.scores(q,[10001,10002,10004]))
 
 
