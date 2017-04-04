514ce32beb825dc390050fdf3bb58ca1135d8746
cspoh <pchunsio@gmail.com>
2017-04-04 22:11:42 +0100

    Added Regressor

--- python/XGBoostRanker.py
+++ python/XGBoostRanker.py
@@ -11,28 +11,90 @@ class XGBoostRanker():
     def __init__(self):
         self.y_parameter = 'relevance'
         self.x_parameter = [
-                # 'tfidf_product_title', # 0.6383
-                # 'tfidf_product_brand',
-                # 'tfidf_product_description',
-                # 'tfidf_attr_json',
-                # 'tfidf_expanded_product_title',
-                # 'tfidf_expanded_product_brand',
-                # 'tfidf_expanded_product_description',
-                # 'tfidf_expanded_attr_json',
-                'doc2vec_product_title', #0.8139
-                # 'doc2vec_product_brand',
-                # 'doc2vec_product_description',
-                # 'doc2vec_attr_json',
-                # 'doc2vec_expanded_product_title',
-                # 'doc2vec_expanded_product_brand',
-                # 'doc2vec_expanded_product_description',
-                # 'doc2vec_expanded_attr_json',
-                # 'bm25',
-                # 'bm25expandedquery',
-                # 'len_product_title',
-                # 'len_product_description',
-                # 'len_brand',
-                # 'len_search_term',
+                'tfidf_product_title', # 0.6383
+                'tfidf_product_brand',
+                'tfidf_product_description',
+                'tfidf_attr_json',
+                'tfidf_expanded_product_title',
+                'tfidf_expanded_product_brand',
+                'tfidf_expanded_product_description',
+                'tfidf_expanded_attr_json',
+                'doc2vec_product_title', #0.62808
+                'doc2vec_product_brand',
+                'doc2vec_product_description',
+                'doc2vec_attr_json',
+                'doc2vec_expanded_product_title',
+                'doc2vec_expanded_product_brand',
+                'doc2vec_expanded_product_description',
+                'doc2vec_expanded_attr_json',
+                'bm25',
+                'bm25expandedquery',
+                'len_product_title',
+                'len_product_description',
+                'len_brand',
+                'len_search_term',
+                'brand_exist',
+                'color_exist',
+                # 'color1hot_almond',
+                # 'color1hot_aluminum',
+                # 'color1hot_beige',
+                # 'color1hot_biscuit',
+                # 'color1hot_bisque',
+                # 'color1hot_black',
+                # 'color1hot_blue',
+                # 'color1hot_bone',
+                # 'color1hot_brass',
+                # 'color1hot_bronze',
+                # 'color1hot_brown',
+                # 'color1hot_cedar',
+                # 'color1hot_charcoal',
+                # 'color1hot_cherry',
+                # 'color1hot_chestnut',
+                # 'color1hot_chrome',
+                # 'color1hot_clear',
+                # 'color1hot_color',
+                # 'color1hot_concrete',
+                # 'color1hot_copper',
+                # 'color1hot_cream',
+                # 'color1hot_daylight',
+                # 'color1hot_espresso',
+                # 'color1hot_gold',
+                # 'color1hot_gray',
+                # 'color1hot_green',
+                # 'color1hot_grey',
+                # 'color1hot_ivory',
+                # 'color1hot_java',
+                # 'color1hot_linen',
+                # 'color1hot_mahogany',
+                # 'color1hot_metallic',
+                # 'color1hot_mocha',
+                # 'color1hot_multi',
+                # 'color1hot_natural',
+                # 'color1hot_nickel',
+                # 'color1hot_oak',
+                # 'color1hot_orange',
+                # 'color1hot_pewter',
+                # 'color1hot_pink',
+                # 'color1hot_platinum',
+                # 'color1hot_primed',
+                # 'color1hot_purple',
+                # 'color1hot_red',
+                # 'color1hot_sand',
+                # 'color1hot_silver',
+                # 'color1hot_slate',
+                # 'color1hot_stainless',
+                # 'color1hot_steel',
+                # 'color1hot_tan',
+                # 'color1hot_teal',
+                # 'color1hot_unfinished',
+                # 'color1hot_walnut',
+                # 'color1hot_white',
+                # 'color1hot_wood',
+                # 'color1hot_yellow',
+                'wm_product_description',
+                'wm_product_title',
+                'wm_product_brand',
+                'wm_attr_json',
             ]
 
     def train(self, trainDF):
@@ -60,6 +122,10 @@ class XGBoostRanker():
         y_train = trainDF[self.y_parameter]
         x_train = trainDF[self.x_parameter]
 
+        y_train = [y/3 for y in y_train]
+
+        # print(y_train[:10])
+
         RMSE = make_scorer(rmse, greater_is_better=False)
 
         print ("No of features in input matrix: %d" % len(x_train.columns))
@@ -71,13 +137,18 @@ class XGBoostRanker():
         # # xgdmat = DMatrix(x_train, y_train)
         # # self._model = train(optimised_params, xgdmat, num_boost_round=432,  verbose_eval=False)
         self._model = XGBRegressor(learning_rate=0.25, silent=False, objective="reg:linear", nthread=-1, gamma=0,
-                                   min_child_weight=1, max_delta_step=0, subsample=1, colsample_bytree=3,
+                                   min_child_weight=1, max_delta_step=0, subsample=1, colsample_bytree=1,
                                    colsample_bylevel=1, reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
-                                   base_score=0.5, seed=0, missing=None)
+                                   base_score=0.5, seed=0, missing=None, max_depth=5)
 
         self._model.fit(x_train, y_train)
 
         y_pred = self._model.predict(x_train)
+        y_pred = [y*3 for y in y_pred]
+        y_train = [y*3 for y in y_train]
+
+        for i in range(30):
+            print("Gold: %.2f  Pred: %.2f" %(y_train[i], y_pred[i]))
 
         print("RMSE: ", rmse(y_train, y_pred))
 
@@ -133,13 +204,13 @@ def rmse(y_gold, y_pred):
 
 if __name__ == "__main__":
     reader = HomeDepotReader()
-    # feature_df = reader.getBasicDataFrame("../data/features.csv")
+    feature_df = reader.getBasicDataFrame("../data/features_full.csv")
     # feature_df = reader.getBasicDataFrame("../data/features_Doc2Vec.csv")
-    feature_df = reader.getBasicDataFrame("../data/features_Doc2Vec_retrain.csv")
+    # feature_df = reader.getBasicDataFrame("../data/features_Doc2Vec_retrain.csv")
 
     print("####  Running: XGBoostRanker.runXGBoostRanker() ####")
     xgb = XGBoostRanker()
-    xgb.train(feature_df)
-    # xgb.train_Regressor(feature_df)
+    # xgb.train(feature_df)
+    xgb.train_Regressor(feature_df)
     # xgb.gridSearch(feature_df)
 
